1e5e76382fd31c35a777ecbaab06e4c68b3115d7
Brian Duff <cairndubh@gmail.com>
2026-06-02 23:16:27 -0700

    propagate startup flags to repobin

--- .buildkite/steps/ci-env.sh
+++ .buildkite/steps/ci-env.sh
@@ -4,18 +4,22 @@ OS_TYPE=$(uname -s | tr '[:upper:]' '[:lower:]')
 
 export REPOBIN_BAZEL_FLAGS="--config=ci-${OS_TYPE}"
 
+BAZEL_STARTUP_FLAGS=""
+
+STARTUP_RC=".ci.${OS_TYPE}.startup.bazelrc"
+if [[ -f "$STARTUP_RC" ]]; then
+  BAZEL_STARTUP_FLAGS="--bazelrc=$STARTUP_RC"
+fi
+
+export REPOBIN_BAZEL_STARTUP_FLAGS="$BAZEL_STARTUP_FLAGS"
+
 # Wrap bazel and pass in ci configuration
 bazel() {
   local subcommand="$1"
   shift
 
-  local bazelrc_arg=""
-  if [[ -f ".ci.${OS_TYPE}.startup.bazelrc" ]]; then
-    bazelrc_arg="--bazelrc=.ci.${OS_TYPE}.startup.bazelrc"
-  fi
-
   command bazel \
-    $bazelrc_arg \
+    $BAZEL_STARTUP_FLAGS \
     "$subcommand" \
     --config="ci-${OS_TYPE}" \
     "$@"
